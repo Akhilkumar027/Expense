938290d47397c7587bbbf6136b98decf90087c0b
Akhilkumar027 <168632932+Akhilkumar027@users.noreply.github.com>
2026-06-09 02:25:52 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "savings-app-v1";
+
+
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
